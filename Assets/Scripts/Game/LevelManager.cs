5d35acc02c3237f621a7d515ee77f5150faae9b8
sahanyaL <piyumdi.yunash@gmail.com>
2025-02-28 15:18:15 +0530

    fixed the level manager issue

--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -1,3 +1,4 @@
+/* prefabs
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -72,4 +73,92 @@ public class LevelManager : MonoBehaviour
     }
 
 
+}
+*/
+
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    [SerializeField] private GameObject[] levelPrefabs; // Assign level prefabs in Inspector
+    [SerializeField] private Transform levelContainer;  // Parent object under GameUI > UI
+    [SerializeField] private GameObject nextLevelUIPanel;
+    
+    private GameObject currentLevel;
+    private int currentLevelIndex = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        LoadLevel(currentLevelIndex); // Load the first level at the start
+    }
+
+    public void LoadNextLevelUI()
+    {
+        Invoke("ShowNextLevelPanel", 5f);
+    }
+
+    private void ShowNextLevelPanel()
+    {
+        if (nextLevelUIPanel != null)
+        {
+            nextLevelUIPanel.SetActive(true);
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        if (nextLevelUIPanel != null)
+        {
+            nextLevelUIPanel.SetActive(false);
+        }
+
+        if (currentLevel != null)
+        {
+            Destroy(currentLevel); // Remove the current level
+        }
+
+        currentLevelIndex++;
+
+        if (currentLevelIndex < levelPrefabs.Length)
+        {
+            LoadLevel(currentLevelIndex);
+        }
+        else
+        {
+            Debug.Log("No more levels! Game Completed.");
+        }
+    }
+
+    private void LoadLevel(int index)
+    {
+        if (index >= levelPrefabs.Length)
+        {
+            Debug.LogError("Level index out of range!");
+            return;
+        }
+
+        currentLevel = Instantiate(levelPrefabs[index], levelContainer);
+        Debug.Log("Loaded Level: " + (index + 1));
+
+        // Optional: Increase timer when new level is loaded
+        GameTimer timer = FindObjectOfType<GameTimer>();
+        if (timer != null)
+        {
+            timer.IncreaseTimer(5);
+        }
+    }
 }
